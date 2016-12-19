77f80ad8751920be447fc9385593728647cb65af
Cameron Moeller <cameron.moeller@gmail.com>
2016-12-19 13:23:33 -0500

    cleaned up secret_santa.py

--- secret_santa.py
+++ secret_santa.py
@@ -1,7 +1,8 @@
 import random
 
 
-fail_dict = {'a':0, 'b':0, 'c':0, 'd':0, 'e':0, 'f':0,'g':0,'h':0, 'i':0, 'j':0}
+fail_dict = {'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0,
+             'f': 0, 'g': 0, 'h': 0, 'i': 0, 'j': 0}
 
 
 def get_number():
@@ -31,6 +32,7 @@ def get_pairs(list_names):
         recipient = random.choice(available_picks)
         available_picks.remove(recipient)
         if element == recipient:
+            # Uncomment this line if you'd like to see who picked own name
             # print("{} picked their own name.".format(element))
             fail_dict[element] += 1
             fail_count += 1
@@ -46,18 +48,25 @@ def get_pairs(list_names):
 
 
 def secret_santa():
+    # Uncomment these lines to run classic version:
+
     # num_players = get_number()
     # list_names = get_names(num_players)
+    # pairs, fail_count = get_pairs(list_names)
+    # for key in pairs:
+    #     print("{} buys a gift for {} ".format(pairs[key], key))
+    # print("fail count: {}".format(fail_count))
+
+    # Comment these lines out if you run the classic version
+
     fail_list = []
     for i in range(100000):
-        pairs, fail_count = get_pairs(['a','b','c','d','e','f','g','h','i','j'])
+        pairs, fail_count = get_pairs(['a', 'b', 'c', 'd', 'e',
+                                      'f', 'g', 'h', 'i', 'j'])
         fail_list.append(fail_count)
     print(sum(fail_list)/len(fail_list))
     print(max(fail_list))
     for key in fail_dict:
         print(key, fail_dict[key]/100000)
-    # pairs, fail_count = get_pairs(list_names)
-    # for key in pairs:
-    #     print("{} buys a gift for {} ".format(pairs[key], key))
-    # print("fail count: {}".format(fail_count))
+
 secret_santa()
